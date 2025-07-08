c5be8e64ad0b33debfccc131342c60c3ca1b1711
Neil <62321203+CestMerNeil@users.noreply.github.com>
2025-07-08 14:15:04 +0800

    Initial Commit

---
+++ Base64_Encoder_Decoder/Base64_Encoder_DecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Base64_Encoder_DecoderApp.swift
+//  Base64_Encoder_Decoder
+//
+//  Created by Ao XIE on 08/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Base64_Encoder_DecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
